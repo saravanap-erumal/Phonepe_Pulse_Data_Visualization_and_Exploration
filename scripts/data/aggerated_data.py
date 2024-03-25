1d6950b5eae520926185554b61f565d7effa1196
saravanap-erumal <164614821+saravanap-erumal@users.noreply.github.com>
2024-03-25 10:56:13 +0530

    Update aggerated_data.py

--- scripts/data/aggerated_data.py
+++ scripts/data/aggerated_data.py
@@ -1,6 +1,7 @@
 import json
 import re
 import pandas as pd
+import streamlit as st
 
 
 def aggregated_data(data):
@@ -26,6 +27,9 @@ def aggregated_data(data):
 
 def tranction_data(path):
     agg_data = open(path)
+    st.write(path)
+    f.open(path)
+    st.write(f)
     agg_data = json.load(agg_data)
     agg_data = agg_data['data']["transactionData"]
     rows = []
